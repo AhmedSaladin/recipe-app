2ab5aed538345122b01fb62bdf77dfda6cddf218
Ahmed Saladin <as.yuri74@gmail.com>
2022-03-31 15:39:05 +0200

    Finish shopping list

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  OnInit,
+  Output,
+  ViewChild,
+} from '@angular/core';
+import { Ingredient } from 'src/app/shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html',
-  styleUrls: ['./shopping-edit.component.css']
+  styleUrls: ['./shopping-edit.component.css'],
 })
 export class ShoppingEditComponent implements OnInit {
+  @ViewChild('name') nameRef?: ElementRef;
+  @ViewChild('amount') amountRef?: ElementRef;
+  constructor() {}
+  @Output('newIngredientAdded') ingredient = new EventEmitter<Ingredient>();
+  ngOnInit(): void {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  addNewIngredient() {
+    const name = this.nameRef?.nativeElement.value;
+    const amount = this.amountRef?.nativeElement.value;
+    this.ingredient.emit(new Ingredient(name, amount));
   }
 
 }
