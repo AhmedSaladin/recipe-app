7062fa09abc4cb907706bc609d6e5bf062f6cc62
Ahmed Saladin <as.yuri74@gmail.com>
2022-03-31 21:00:56 +0200

    Update model

--- src/app/recipes/recipe.model.ts
+++ src/app/recipes/recipe.model.ts
@@ -1,10 +1,17 @@
+import { Ingredient } from '../shared/ingredient.model';
+
 export class Recipe {
   name: String;
   description: String;
   imagePath: String;
-  ingredients?: String;
+  ingredients: Array<Ingredient>;
 
-  constructor(name: String, desc: String, img: String, ingredients?: String) {
+  constructor(
+    name: String,
+    desc: String,
+    img: String,
+    ingredients: Array<Ingredient>
+  ) {
     this.name = name;
     this.description = desc;
     this.imagePath = img;
--- src/app/recipes/service/recipe.service.ts
+++ src/app/recipes/service/recipe.service.ts
@@ -1,4 +1,5 @@
 import { EventEmitter, Injectable } from '@angular/core';
+import { Ingredient } from 'src/app/shared/ingredient.model';
 import { Recipe } from '../recipe.model';
 
 @Injectable({
@@ -8,16 +9,22 @@ export class RecipeService {
   private recipeSelected = new EventEmitter<Recipe>();
   private recipes: Array<Recipe> = [
     new Recipe(
-      'Test 1',
-      'Simple recipe test 1',
-      'https://pixabay.com/get/g98852dcc569a9a2fea22e89f31b96acc94e3fe06585f877ff442d639b331e70d0742536e0523408ae9693e6f80833a972282d39ef7cd30cc11b110579f7f4af360eaa3336eaf362fdaa24ac34a31e6c9_640.jpg'
-    ),
-    new Recipe(
-      'Test 2',
-      'Simple recipe test 2',
-      'https://pixabay.com/get/g470b68444e65ab6362ac6f5e48316356a202c0cdd0bc198864ff92245cc3f03b77b01c0773582cf2713f06ad395d812e714197086f23638080e438115cd077dae336ec66e95448387c756b5533304353_640.jpg'
-    ),
+      'Tasty Schnitzel',
+      'A super-tasty Schnitzel - just awesome!',
+      'https://upload.wikimedia.org/wikipedia/commons/7/72/Schnitzel.JPG',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('French Fries', 20)
+      ]),
+    new Recipe('Big Fat Burger',
+      'What else you need to say?',
+      'https://upload.wikimedia.org/wikipedia/commons/b/be/Burger_King_Angus_Bacon_%26_Cheese_Steak_Burger.jpg',
+      [
+        new Ingredient('Buns', 2),
+        new Ingredient('Meat', 1)
+      ])
   ];
+
   constructor() {}
 
   addNewRecipe(recipe: Recipe) {
