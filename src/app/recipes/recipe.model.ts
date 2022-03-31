9513dd7a88e4c723a94c5d7bf7cb31d479881a37
Ahmed Saladin <as.yuri74@gmail.com>
2022-03-31 14:32:58 +0200

    Fix recipe details structure

--- src/app/recipes/recipe.model.ts
+++ src/app/recipes/recipe.model.ts
@@ -2,10 +2,12 @@ export class Recipe {
   name: String;
   description: String;
   imagePath: String;
+  ingredients?: String;
 
-  constructor(name: String, desc: String, img: String) {
+  constructor(name: String, desc: String, img: String, ingredients?: String) {
     this.name = name;
     this.description = desc;
     this.imagePath = img;
+    this.ingredients = ingredients;
   }
 }
