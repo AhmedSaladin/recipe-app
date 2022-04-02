1e5ac5639364bcc9b0b4840f2890d17ecbb5947d
Ahmed Saladin <as.yuri74@gmail.com>
2022-04-02 16:46:31 +0200

    Fix recipe style

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,6 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Recipe } from '../../recipe.model';
-import { RecipeService } from '../../service/recipe.service';
 
 @Component({
   selector: 'app-recipe-item',
@@ -9,11 +8,8 @@ import { RecipeService } from '../../service/recipe.service';
 })
 export class RecipeItemComponent implements OnInit {
   @Input() recipe?: Recipe;
-  constructor(private recipeService: RecipeService) {}
+  @Input() index?: number;
+  constructor() {}
 
   ngOnInit(): void {}
-
-  onSelected() {
-    this.recipeService.setSingleRecipe(this.recipe);
-  }
 }
